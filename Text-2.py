ffde461e342042bafb431db54545c85261ecf2d1
ITZ-JISAN <127544614+X1X4D-2-0@users.noreply.github.com>
2023-05-15 13:21:53 +0600

    Update Text-2.py

--- Text-2.py
+++ Text-2.py
@@ -470,7 +470,7 @@ def rcrack(uid,pwx,tl):
             "login":"Log In"}
 headers = { 'authority': 'mbasic.facebook.com',
             "method": 'GET',
-            "scheme": 'https
+            "scheme": 'https',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
             'cache-control': 'max-age=0',
